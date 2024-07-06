8afbda525f26a3ac3ee28b8d98ed7d07abc7e5bc
Sangam Arora <arorasam2109@gmail.com>
2024-07-06 15:36:32 +0530

    commited added new

--- client/src/utils/AdminFunctions.js
+++ client/src/utils/AdminFunctions.js
@@ -196,3 +196,20 @@ export const getAllData = async () => {
         throw new Error("Error fetching users");
     }
 };
+export const getAllLogs = async () => {
+    try {
+        const token = localStorage.getItem("authtoken");
+        const response = await axios.get(`${API_URL}logs/getalllogs`, {
+            headers: {
+                "auth-token": token,
+            },
+        });
+
+        const logs = response.data.data
+
+        return logs;
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        throw new Error("Error fetching users");
+    }
+};
